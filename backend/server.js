f4fcf7c339065dda58b47aea60f170bf2c1f83f0
chei7tain <fredrickokwuobi@gmail.com>
2021-12-19 23:10:38 +0100

    add connection to mongodb local

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,11 @@
 import express from "express";
 import mongoose from "mongoose";
 const app = express();
-const port = process.env.PORT || 5000;
 import cors from "cors";
 import data from "./data.js";
+import config from "./config.js";
+console.log(config);
+console.log("hello", mongoose.connect(config.MONGODB_URL));
 
 mongoose
   .connect(config.MONGODB_URL, {
@@ -12,7 +14,9 @@ mongoose
     useUnifiedTopology: true,
     useCreateIndex: true,
   })
+  .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log(err.reason));
+
 app.use(cors());
 app.get("/api/products", (req, res) => {
   res.send(data.products);
@@ -26,6 +30,7 @@ app.get("/api/products/:id", (req, res) => {
     res.status(404).send({ message: "Product not found" });
   }
 });
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on port http://localhost:${port}`);
 });
