ce42546973dc79495ed07d1f21073f832c53d347
chei7tain <fredrickokwuobi@gmail.com>
2021-12-19 18:19:59 +0100

    add styles for product screen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -51,7 +51,7 @@ const ProductScreen = {
                       }
                     </li>
                     <li>
-                      <button id="add-button" class="primary">Add to Cart</button>
+                      <button id="add-button" class="fw primary">Add to Cart</button>
                     </li>
                   </ul>
                 </div>
