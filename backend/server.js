8dfd06d5c5342a17a5e8f84ca0820d8c5aecdf63
chei7tain <fredrickokwuobi@gmail.com>
2021-12-19 15:13:02 +0100

    add babel transpiler

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,15 @@
-const express = require("express");
+// const express = require("express");
+import express from "express";
 const app = express();
-const cors = require("cors");
-const data = require("./data.js");
+const port = process.env.PORT || 5000;
+import cors from "cors";
+import data from "./data.js";
 
 app.use(cors());
 app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
 
-app.listen(5000, () => {
-  console.log(`Server is running on port http://localhost:${5000}`);
+app.listen(port, () => {
+  console.log(`Server is running on port http://localhost:${port}`);
 });
