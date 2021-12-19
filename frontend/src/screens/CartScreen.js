ecd713ffff4f36d1305072db131e97e6b4f1a28d
chei7tain <fredrickokwuobi@gmail.com>
2021-12-19 18:58:25 +0100

    fix addcartItems

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -3,36 +3,43 @@ import { parseRequestUrl } from "../utils";
 import { getCartItems } from "../localStorage";
 import { setCartItem } from "../localStorage";
 
-const addToCart = (item,forceUpdate = false) => {
+const addToCart = (item, forceUpdate = false) => {
   let cartItems = getCartItems();
-  const existItem = cartItems.find((cartItem) => cartItem.product === item.product);
-  if(existItem){
-    cartItems = cartItems.map(cartItem => cartItem.product === existItem.product ? {...cartItem, quantity: cartItem.quantity + item.quantity} : cartItem);
+  const existItem = cartItems.find(
+    (cartItem) => cartItem.product === item.product
+  );
+  if (existItem) {
+    cartItems = cartItems.map((cartItem) =>
+      cartItem.product === existItem.product ? item : cartItem
+    );
+  } else {
+    cartItems = [...cartItems, item];
   }
   setCartItem(cartItems);
-}
+};
 
 const CartScreen = {
   after_render: () => {},
   render: async () => {
     const request = parseRequestUrl();
-    if(request.id){
+    if (request.id) {
       const product = await getProduct(request.id);
       if (product.error) {
         return `<div>${product.error}</div>`;
-      }else{
+      } else {
         addToCart({
-          product:product._id,
-          name:product.name,
-          price:product.price,
-          image:product.image,
-          countInStock:product.countInStock,
-          qty:1
-        })
+          product: product._id,
+          name: product.name,
+          price: product.price,
+          image: product.image,
+          countInStock: product.countInStock,
+          qty: 1,
+        });
       }
-
     }
-    return `<div>CartScreen</div>`;
+    return `<div>CartScreen</div>
+      <div>${getCartItems().length}</div>
+    `;
   },
 };
 export default CartScreen;
