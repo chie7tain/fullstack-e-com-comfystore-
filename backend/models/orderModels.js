26448e5db2e3bd6829e0e26efce6e11fdecde9ee
chei7tain <fredrickokwuobi@gmail.com>
2021-12-20 11:20:20 +0100

    add order schema

---
+++ backend/models/orderModels.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema(
+  {
+    orderItems: [
+      {
+        name: { type: String, required: true },
+        image: { type: String, required: true },
+        qty: { type: Number, required: true },
+        price: { type: Number, required: true },
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+      },
+    ],
+    user: { type: mongoose.Schem.Types.ObjectId, ref: "User", required: true },
+    shipping: {
+      address: String,
+      city: String,
+      postalCode: String,
+      country: String,
+    },
+    payment: {
+      paymentMethod: String,
+      paymentResult: {
+        orderID: String,
+        payerID: string,
+        paymentID: string,
+      },
+    },
+    itemsPrice: Number,
+    taxPrice: Number,
+    shippingPrice: Number,
+    totalPrice: Number,
+    isPaid: { type: Boolean, default: false, required: true },
+    paidAt: Date,
+    isDelivered: { type: Boolean, default: false, required: true },
+    deliveredAt: Date,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Order = mongoose.model("Order", orderSchema);
+export default Order;
